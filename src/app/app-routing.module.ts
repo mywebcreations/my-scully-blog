ba918efed173d53b8c270bde4c5b4534f28514df
Adedoyin Adedipe <mywebcreativities@gmail.com>
2023-11-04 23:54:16 +0100

    SCULLY-14 added ContactComponent to routes in main routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component'
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: 'contact', component: ContactComponent }
